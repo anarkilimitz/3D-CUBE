b539f95c9143ac872836a20db2f0e2960c7a3da2
Evgenii <anarkilimitz@gmail.com>
2025-03-14 10:52:53 +0500

    script.js

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ const rotations = [
 const initialTilt = { x: -15, y: 15 };
 
 cube.addEventListener('click', () => {
+    // Останавливаем анимацию
+    cube.style.animationPlayState = 'paused';
+
     currentRotation = (currentRotation + 1) % rotations.length;
     const { x, y } = rotations[currentRotation];
     
@@ -22,4 +25,9 @@ cube.addEventListener('click', () => {
         rotateX(${x + initialTilt.x}deg) 
         rotateY(${y + initialTilt.y}deg)
     `;
+
+    // Возобновляем анимацию через 1 секунду (после завершения поворота)
+    setTimeout(() => {
+        cube.style.animationPlayState = 'running';
+    }, 1000);
 });
